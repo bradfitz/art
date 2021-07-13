98f146f2b093d2d154ecb79e0925af9f65f9ae1c
julianknodt <julianknodt@gmail.com>
2021-07-12 23:37:39 -0700

    switch to ipprefix

--- art_test.go
+++ art_test.go
@@ -7,16 +7,19 @@ package art
 
 import (
 	"math/rand"
+	"net"
 	"reflect"
 	"testing"
+
+	"inet.af/netaddr"
 )
 
 func TestBaseIndex(t *testing.T) {
 	tests := []struct {
 		w    int
-		a    uint64
+		a    uint32
 		l    int
-		want uint64
+		want uint32
 	}{
 		{4, 0, 0, 1},
 		{4, 0, 1, 2},
@@ -53,18 +56,20 @@ type route4b struct {
 	l uint8 // prefix len
 }
 
-func (r route4b) RouteParams() RouteParams {
-	return RouteParams{
-		Width: 4,
-		Addr:  uint64(r.a),
-		Len:   int(r.l),
-	}
+func (r route4b) IPPrefix() netaddr.IPPrefix {
+	return netaddr.IPPrefixFrom(
+		netaddr.IPFrom4([4]byte{0, 0, 0, r.a}),
+		r.l,
+	)
+}
+
+func (r route4b) Equals(other Route) bool {
+	r4b, ok := other.(route4b)
+	return ok && r == r4b
 }
 
 func newSingleLevelTestTable() *Table {
-	x := NewTable(4)
-	x.w = 4
-	return x
+	return NewTable([]int{4})
 }
 
 var _ Route = route4b{}
@@ -74,55 +79,58 @@ func TestInsertSingleLevel(t *testing.T) {
 
 	// Figure 3-1.
 	r1 := route4b{12, 2}
-	if !x.insertSingleLevel(r1) {
+	if !x.Insert(r1) {
 		t.Errorf("insert %v failed", r1)
 	}
 	want := newSingleLevelTestTable()
+	want.root.ref++
 	for _, i := range []int{7, 14, 15, 28, 29, 30, 31} {
-		want.r[i] = r1
+		want.root.r[i] = r1
 	}
-	if !reflect.DeepEqual(x, want) {
-		t.Errorf("wrong after 1st step\n got: %v\nwant: %v\n", x, want)
+	if !reflect.DeepEqual(x.root, want.root) {
+		t.Errorf("wrong after 1st step\n got: %v\nwant: %v\n", x.root, want.root)
 	}
 
 	// Figure 3-2. ("Now assume we insert a route to prefix 14/3")
 	r2 := route4b{14, 3}
-	if !x.insertSingleLevel(r2) {
+	if !x.Insert(r2) {
 		t.Errorf("insert %v failed", r2)
 	}
 	for _, i := range []int{15, 30, 31} {
-		want.r[i] = r2
+		want.root.r[i] = r2
 	}
+	want.root.ref++
 	if !reflect.DeepEqual(x, want) {
 		t.Errorf("wrong after 2nd step\n got: %v\nwant: %v\n", x, want)
 	}
 
 	// Figure 3-3. ("Now assume we insert a route to prefix 8/1")
 	r3 := route4b{8, 1}
-	if !x.insertSingleLevel(r3) {
+	if !x.Insert(r3) {
 		t.Errorf("insert %v failed", r3)
 	}
+	want.root.ref++
 	for _, i := range []int{3, 6, 12, 13, 24, 25, 26, 27} {
-		want.r[i] = r3
+		want.root.r[i] = r3
 	}
-	if !reflect.DeepEqual(x, want) {
-		t.Errorf("wrong after 3rd step\n got: %v\nwant: %v\n", x, want)
+	if !reflect.DeepEqual(x.root, want.root) {
+		t.Errorf("wrong after 3rd step\n got: %v\nwant: %v\n", x.root, want.root)
 	}
 }
 
 // testTable returns the example table set up before section 2.1.2 of the paper.
 func testTable() *Table {
 	x := newSingleLevelTestTable()
-	x.insertSingleLevel(route4b{12, 2})
-	x.insertSingleLevel(route4b{14, 3})
-	x.insertSingleLevel(route4b{8, 1})
+	x.Insert(route4b{12, 2})
+	x.Insert(route4b{14, 3})
+	x.Insert(route4b{8, 1})
 	return x
 }
 
 func TestLookupSingleLevel(t *testing.T) {
 	x := testTable()
 	for _, tt := range []struct {
-		addr uint64
+		addr uint32
 		want Route
 	}{
 		{0, nil},
@@ -139,7 +147,7 @@ func TestLookupSingleLevel(t *testing.T) {
 		{14, route4b{14, 3}},
 		{15, route4b{14, 3}},
 	} {
-		got, _ := x.lookupSingleLevel(tt.addr)
+		got, _ := x.root.lookupSingle(4, tt.addr)
 		if got != tt.want {
 			t.Errorf("lookup(addr=%v) = %v; want %v", tt.addr, got, tt.want)
 		}
@@ -148,7 +156,7 @@ func TestLookupSingleLevel(t *testing.T) {
 
 func TestDeleteSingleLevel(t *testing.T) {
 	x := testTable()
-	old, ok := x.deleteSingleLevel(RouteParams{Width: 4, Addr: 12, Len: 2})
+	old, ok := x.Delete(netaddr.MustParseIPPrefix("0.0.0.12/2"))
 	if !ok {
 		t.Fatal("didn't delete")
 	}
@@ -162,7 +170,7 @@ func TestDeleteSingleLevel(t *testing.T) {
 	// 8/1 and 14/3 in them. Instead, do what the paper probably
 	// meant to get back to figure 3-2:
 	x = testTable()
-	old, ok = x.deleteSingleLevel(RouteParams{Width: 4, Addr: 8, Len: 1})
+	old, ok = x.Delete(netaddr.MustParseIPPrefix("0.0.0.8/1"))
 	if !ok {
 		t.Fatal("didn't delete")
 	}
@@ -170,7 +178,7 @@ func TestDeleteSingleLevel(t *testing.T) {
 		t.Fatalf("deleted %v; want %v", old, want)
 	}
 	want := testTable()
-	want.r = []Route{
+	want.root.r = []Route{
 		7:  route4b{12, 2},
 		14: route4b{12, 2},
 		28: route4b{12, 2},
@@ -179,52 +187,60 @@ func TestDeleteSingleLevel(t *testing.T) {
 		30: route4b{14, 3},
 		31: route4b{14, 3},
 	}
+	want.root.ref--
 	if !reflect.DeepEqual(x, want) {
-		t.Errorf("not like Figure 3-2:\n got: %v\nwant: %v\n", x, want)
+		t.Errorf("not like Figure 3-2:\n got: %v\nwant: %v\n", x.root, want.root)
 	}
 }
 
 func newIPv4Table_8() *Table {
-	t := NewTable(8)
-	t.sl = []int{8, 8, 8, 8}
-	t.w = 32
-	return t
+	return NewTable([]int{8, 8, 8, 8})
 }
 
 func newIPv4Table_16_8() *Table {
-	t := NewTable(16)
-	t.sl = []int{16, 8, 8}
-	t.w = 32
-	return t
+	return NewTable([]int{16, 8, 8})
 }
 
 type testRoute struct {
-	rp  RouteParams
+	ipp netaddr.IPPrefix
 	val interface{}
 }
 
-func (tr testRoute) RouteParams() RouteParams { return tr.rp }
+func (tr testRoute) IPPrefix() netaddr.IPPrefix { return tr.ipp }
+
+func (tr testRoute) Equals(r Route) bool {
+	tr2, ok := r.(testRoute)
+	return ok && tr2.val == tr.val && tr.ipp == tr2.ipp
+}
 
 func genTestRoutes(width, num int) []Route {
 	var routes []Route
 	rand.Seed(1)
-	dup := map[RouteParams]bool{}
+	ipps := map[netaddr.IPPrefix]bool{}
+	bytesPer := 16
+	if width <= 32 {
+		bytesPer = 4
+	}
 	for i := 0; i < num; i++ {
-		var rp RouteParams
+		var ipp netaddr.IPPrefix
 		for {
-			rp = RouteParams{
-				Width: width,
-				Len:   rand.Intn(width + 1),
+			length := uint8(rand.Intn(width + 1))
+			addr := make([]byte, bytesPer)
+			for pl := 0; pl < int(length); pl++ {
+				addr[pl/8] |= byte(rand.Intn(2)) << (pl % 8)
 			}
-			for pl := 0; pl < rp.Len; pl++ {
-				rp.Addr |= uint64(rand.Intn(2)) << ((width - 1) - pl)
+			ip, ok := netaddr.FromStdIP(net.IP(addr))
+			if !ok {
+				panic("Failed to created IP")
 			}
-			if !dup[rp] {
-				dup[rp] = true
-				break
+			ipp = netaddr.IPPrefixFrom(ip, length)
+			if seen := ipps[ipp]; seen {
+				continue
 			}
+			ipps[ipp] = true
+			break
 		}
-		routes = append(routes, testRoute{rp, i})
+		routes = append(routes, testRoute{ipp, i})
 	}
 	return routes
 }
@@ -237,22 +253,22 @@ func TestInsertDeleteSingle4bit(t *testing.T) {
 		})
 		x := newSingleLevelTestTable()
 		for i, r := range routes {
-			preInsert := x.clone()
-			if !x.insertSingleLevel(r) {
+			preInsert := x.Clone()
+			if !x.Insert(r) {
 				t.Fatalf("failed to insert %d, %+v", i, r)
 			}
-			rp := r.RouteParams()
-			del, ok := x.deleteSingleLevel(rp)
+			ipp := r.IPPrefix()
+			del, ok := x.Delete(ipp)
 			if !ok {
-				t.Fatalf("failed to delete %d, %+v", i, rp)
+				t.Fatalf("failed to delete %d, %+v", i, ipp)
 			}
-			if del != r {
+			if !del.Equals(r) {
 				t.Fatalf("delete of %d deleted %v, want %v", i, del, r)
 			}
 			if !reflect.DeepEqual(x, preInsert) {
-				t.Fatalf("delete of %d (%+v) didn't return table to prior state\n now: %v\n was: %v\n", i, rp, x, preInsert)
+				t.Fatalf("delete of %d (%+v) didn't return table to prior state\n now: %v\n was: %v\n", i, ipp, x, preInsert)
 			}
-			if !x.insertSingleLevel(r) {
+			if !x.Insert(r) {
 				t.Fatalf("failed to re-insert %d, %+v", i, r)
 			}
 		}
@@ -260,15 +276,26 @@ func TestInsertDeleteSingle4bit(t *testing.T) {
 }
 
 func TestMultiIPv4_stride8(t *testing.T) {
-	testMultiIPv4(t, newIPv4Table_8)
+	testMulti(t, newIPv4Table_8, 32)
 }
 
 func TestMultiIPv4_stride16_8(t *testing.T) {
-	testMultiIPv4(t, newIPv4Table_16_8)
+	testMulti(t, newIPv4Table_16_8, 32)
 }
 
-func testMultiIPv4(t *testing.T, newTable func() *Table) {
-	routes := genTestRoutes(32, 100)
+func TestMultiIPv6_stride8(t *testing.T) {
+	testMulti(t, func() *Table {
+		return NewTable([]int{
+			8, 8, 8, 8,
+			8, 8, 8, 8,
+			8, 8, 8, 8,
+			8, 8, 8, 8,
+		})
+	}, 128)
+}
+
+func testMulti(t *testing.T, newTable func() *Table, width int) {
+	routes := genTestRoutes(width, 100)
 	numShuffle := 10
 	if testing.Short() {
 		numShuffle = 2
@@ -280,36 +307,36 @@ func testMultiIPv4(t *testing.T, newTable func() *Table) {
 		})
 		x := newTable()
 		for i, r := range routes {
-			rp := r.RouteParams()
-			gotBefore, _ := x.Lookup(rp.Addr)
+			ipp := r.IPPrefix()
+			gotBefore, _ := x.Lookup(ipp.IP())
 
-			preInsert := x.clone()
+			preInsert := x.Clone()
 			if !x.Insert(r) {
 				t.Fatalf("failed to insert %d, %+v", i, r)
 			}
 
-			got, ok := x.Lookup(rp.Addr)
+			got, ok := x.Lookup(ipp.IP())
 			if !ok {
-				t.Fatalf("i=%d; Lookup(%d) failed (%+v)", i, rp.Addr, rp)
+				t.Fatalf("i=%d; Lookup(%v) failed (%+v)", i, ipp.IP(), ipp)
 			}
 
 			want := r
-			if gotBefore != nil && gotBefore.(testRoute).rp.Len > rp.Len {
+			if gotBefore != nil && gotBefore.(testRoute).ipp.Bits() > ipp.Bits() {
 				want = gotBefore
 			}
-			if got != want {
-				t.Fatalf("i=%d; Lookup(%d) got %v; want %v", i, rp.Addr, got, want)
+			if !got.Equals(want) {
+				t.Fatalf("i=%d; Lookup(%v) got %v; want %v", i, ipp.IP(), got, want)
 			}
 
-			del, ok := x.Delete(rp)
+			del, ok := x.Delete(ipp)
 			if !ok {
-				t.Fatalf("failed to delete %d, %+v", i, rp)
+				t.Fatalf("failed to delete %d, %+v", i, ipp)
 			}
-			if del != r {
+			if !del.Equals(r) {
 				t.Fatalf("delete of %d deleted %v, want %v", i, del, r)
 			}
 			if !reflect.DeepEqual(x, preInsert) {
-				t.Fatalf("delete of %d (%+v) didn't return table to prior state\n now: %v\n was: %v\n", i, rp, x, preInsert)
+				t.Fatalf("delete of %d (%+v) didn't return table to prior state\n now: %v\n was: %v\n", i, ipp, x, preInsert)
 			}
 			if !x.Insert(r) {
 				t.Fatalf("failed to re-insert %d, %+v", i, r)
@@ -328,7 +355,7 @@ func benchInsertRemoveIPv4(b *testing.B, newTable func() *Table) {
 		if !t.Insert(v) {
 			b.Error("Insertion failed")
 		}
-		if _, ok := t.Delete(v.RouteParams()); !ok {
+		if _, ok := t.Delete(v.IPPrefix()); !ok {
 			b.Error("Removal failed")
 		}
 	}
@@ -364,7 +391,7 @@ func benchSearchIPv4(b *testing.B, newTable func() *Table) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		v := routes[i%uniq]
-		if _, ok := t.Lookup(v.RouteParams().Addr); !ok {
+		if _, ok := t.Lookup(v.IPPrefix().IP()); !ok {
 			b.Error("Lookup failed")
 		}
 	}
